6fa48d0ff45c688a1a4557c14af521d26d12a29a
Mario Hewera <mh@villaester.de>
2025-08-19 07:48:14 +0200

    Adds content template for vmmapicon
    Creates a content template file for the vmmapicon element,
    displaying content based on the 'content' property.
    The template includes copyright and license information.

--- plugins/system/ytvmmapicon/element/form_vmmapicon/templates/content.php
+++ plugins/system/ytvmmapicon/element/form_vmmapicon/templates/content.php
@@ -1,19 +1,21 @@
-<?php if ($props['content']) : ?>
-/**  
- * 
- * 
- * \ \    / /  \/  |  \/  | 
- *  \ \  / /| \  / | \  / | 
- *   \ \/ / | |\/| | |\/| | 
- *    \  /  | |  | | |  | | 
- *     \/   |_|  |_|_|  |_| Villaester Moderne Medien GmbH * * @package Joomla.Component  
+<?php
+/**
+ *
+ *
+ * \ \    / /  \/  |  \/  |
+ *  \ \  / /| \  / | \  / |
+ *   \ \/ / | |\/| | |\/| |
+ *    \  /  | |  | | |  | |
+ *     \/   |_|  |_|_|  |_| Villaester Moderne Medien GmbH * * @package Joomla.Component
  * @subpackage  com_vmmapicon
- * @copyright   Copyright (C) 2025 Villaester Moderne Medien  
- * @author      Mario Hewera & Kiki Schuelling  
- * @license     GNU General Public License version 2 or later  
- * @author      VMM Development Team  
- * @link        https://villaester.de  
- * @version     1.0.0  
+ * @copyright   Copyright (C) 2025 Villaester Moderne Medien
+ * @author      Mario Hewera & Kiki Schuelling
+ * @license     GNU General Public License version 2 or later
+ * @author      VMM Development Team
+ * @link        https://villaester.de
+ * @version     1.0.0
  */
+?>
+<?php if ($props['content']) : ?>
 <div><?= $props['content'] ?></div>
 <?php endif ?>
